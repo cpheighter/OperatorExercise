920e57e030a767eac5871cc2093819f98a0311be
cpheighter <cheighterjr@gmail.com>
2022-07-06 15:41:28 -0400

    added more operations

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,6 +4,11 @@
 int a = 17;
 int b = 4;
 
+Console.WriteLine(Methods.Add(a, b));
+Console.WriteLine(Methods.Subtract(a, b));
+Console.WriteLine(Methods.Multiply(a, b));
+Console.WriteLine(Methods.Divide(a, b));
+//------------------------------------------------
 int quotient = a / b;
 int remainder = a % b;
 
