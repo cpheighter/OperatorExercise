4fa0f81db9da027de24fe195241e506c0578b384
cpheighter <cheighterjr@gmail.com>
2022-07-06 14:21:09 -0400

    1st init

---
+++ OperatorExercise/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Methods
+    {
+        public static double Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static double Multiply(int a, int b)
+        {
+            return a * b;
+            
+        }
+        public static double Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static double Modulus(int a, int b)
+        {
+            return a % b;
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+    }
+}
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using OperatorExercise;
+
+//Exercise 1
+int a = 17;
+int b = 4;
+
+int quotient = a / b;
+int remainder = a % b;
+
+Console.WriteLine($"17/4 is {quotient} reminder {remainder}");
+//------------------------------------------------
+// Exercise 2
+
+Console.Write("Enter the radius of the circle: ");
+    var radius = double.Parse(Console.ReadLine());
+Console.WriteLine(Methods.AreaOfCircle(radius));
